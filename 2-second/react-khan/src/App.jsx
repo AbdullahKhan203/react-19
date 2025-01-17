539454eb38be9c7e8412f037242bb54067f7bcf6
AbdullahKhan203 <hk7485966@gmail.com>
2025-01-17 22:36:36 +0500

    created of nexflix card

--- 2-second/react-khan/src/App.jsx
+++ 2-second/react-khan/src/App.jsx
@@ -1,3 +1,26 @@
+import React from "react"
+// these tow App function work same in backside
 export const App=()=>{
-  return <h1>Hello Khans</h1>
+return <div>
+<img src="netflix.jpg" alt="image" width="40%" height="40%" />
+<h1>Name : Queen of Tears</h1>
+<h3>Ratings: 8.2</h3>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum, commodi provident laborum, quaerat quibusdam necessitatibus nemo quasi fugiat, eveniet omnis tempora quae possimus atque doloremque perspiciatis neque ab magni.</p>
+</div>
 }
+
+
+
+
+
+
+
+// export const App=()=>{
+//   return React.createElement("h1",null,"Hello world by createElement");
+// }
+
+
+
+
+// react work like this is backside
+// React.createElement("h1",null,"Hello world by createElement")
